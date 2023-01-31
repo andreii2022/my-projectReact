aee9a5adda464a4f50ee776634c32ce5adf90631
Andrii Nikulin <nikulin.and90@gmail.com>
2023-01-31 22:09:18 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
-// import './App.css';
-import './main.scss';
+import Header from './Layaout/Header/Header';
+import Footer from './Layaout/Footer/Footer';
+import Main from './Pages/Main';
+import React from 'react';
+import './Main.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Layaout/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        
+    <header className='header'>
+        <div className='container'>
+            <nav>
+                <ul>
+                    <li className='header__item' >Q</li>
+                    <li className='header__item' >Q</li>
+                    <li className='header__item' >Q</li>
+                    <li className='header__item' >Q</li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>       
+    
+    );
+};
+
+export default Header;
