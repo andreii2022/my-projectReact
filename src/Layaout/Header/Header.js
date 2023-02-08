4ed238a1ae7484467e6db4c279a471b08f282443
Andrii Nikulin <nikulin.and90@gmail.com>
2023-02-08 19:59:59 +0200

    adding logic video player

--- src/Layaout/Header/Header.js
+++ src/Layaout/Header/Header.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import MainButton from '../../Pages/MainButton/MainButton';
 import avatar from '../../images/Avatar.jpg'
 import icons from '../../images/sprites/sprite.svg'
 // import { ReactComponent as IconOne } from "../../images/iconOne.svg";
 
 const Header = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const togglePlay = () => {
+        if (isPlaying) {
+            videoRef.current.pause();
+        } else {
+            videoRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
     return (
     <header className='header'>
         <div className="container">
@@ -16,7 +27,11 @@ const Header = () => {
             <MainButton styleClass="btn header__btn"/>
 
             <div className="header__video">
-                <button className="header__play"></button>
+            <video width="100%" height="100%" ref={videoRef} controls>
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+            </video>
+                {/* <button className="header__play" onClick={togglePlay}></button> */}
+                <button className={isPlaying ? 'header__play' : 'header__play pause'} onClick={togglePlay}></button>
             </div>
             <div className="header__post">
                 <div className="header__post-inner">
